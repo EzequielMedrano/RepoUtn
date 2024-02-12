6368c32803bbc03d8a49b103a486d493796355cc
Ezequiel Leonel Medrano Mamani <medranoezequiel07@gmail.com>
2024-02-12 14:47:25 -0300

    parcial amazin casi completo

--- Pizzas/src/Library.hs
+++ Pizzas/src/Library.hs
@@ -9,7 +9,7 @@ data Persona = UnaPersona{
   felicidad::Number,
   librosQueAdquirio::[Libro],
   librosLeidos::[Libro]
-}deriving (Eq,Show) 
+}deriving (Eq,Show)
 
 data Libro = UnLibro{
   titulo :: String,
@@ -17,6 +17,9 @@ data Libro = UnLibro{
   cantPaginas :: Number,
   efecto:: Efecto -- cómo afecta el género a las personas que lo lean.
 }deriving (Eq, Show)
+
+condicionParaQueSeConsidereLeido::Persona->Libro->Libro->Bool
+condicionParaQueSeConsidereLeido persona libro1 libro2  = titulo libro1 == titulo libro2 -- && loEscribio libro1 == persona
 
 aumentaFelicidad ::Number->Persona->Persona
 aumentaFelicidad valor persona = persona{
@@ -26,8 +29,9 @@ adquiereLibro::Libro->Persona->Persona
 adquiereLibro libro persona = persona {
   librosLeidos = librosLeidos persona ++ [libro]
 }
-condicionParaQueSeConsidereLeido::Persona->Libro->Libro->Bool
-condicionParaQueSeConsidereLeido persona libro1 libro2  = titulo libro1 == titulo libro2 && (loEscribio libro1) == persona
+cantidadDePaginasMayorQue libro cantidad = cantPaginas libro > cantidad
+cantidadDePaginasMenorQue libro cantidad = cantPaginas libro < cantidad
+
 
 romero :: Persona
 romero = UnaPersona "romero" 100 [patoruzito] [patoruzito]
@@ -51,12 +55,36 @@ libroTerror persona = persona{
   librosQueAdquirio = []
 }
 
+--PARTE C
 leeUnLibro::Libro->Efecto->Persona->Persona
-leeUnLibro libro efecto persona = (adquiereLibro libro . efecto ) persona 
+leeUnLibro libro efecto persona = (adquiereLibro libro . efecto ) persona
 
 
 sePusoAlDiaConLosLibros::Libro->Persona->Bool
 sePusoAlDiaConLosLibros libro persona = any (condicionParaQueSeConsidereLeido persona libro ) (librosLeidos persona)
 
+fanaticoDelEscritor::Libro->Persona->Bool
+fanaticoDelEscritor libro persona = all(\libroleido-> loEscribio libroleido == persona) (librosLeidos persona)
+
+-- Una persona no puede ponerse al dia con una lista infinita de libros porque no hay un tope o algo que frene esa busqueda como podria
+-- ser un take , Esto es posible debido a la evaluación perezosa.
 
 
+--PARTE D
+
+type TipoDeLibro = Libro -> Bool
+cuento :: TipoDeLibro
+cuento libro = cantidadDePaginasMenorQue libro 100
+
+novelaCorta :: TipoDeLibro
+novelaCorta libro = cantidadDePaginasMayorQue libro 100 && cantidadDePaginasMenorQue libro 200
+
+novela :: TipoDeLibro
+novela libro = cantidadDePaginasMayorQue libro 200
+
+librosAdquiridosSegunElTipo :: Persona -> TipoDeLibro -> [Libro]
+librosAdquiridosSegunElTipo persona tipoDeLibro = filter tipoDeLibro (librosQueAdquirio persona)
+
+
+titulosDeLosLibrosAdquiridos::Persona->TipoDeLibro->[String]
+titulosDeLosLibrosAdquiridos persona tipoDeLibro = map titulo (librosAdquiridosSegunElTipo persona tipoDeLibro)
